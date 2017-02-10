8386165188b33fdea37fee4663c536334dc2e7d1
Aaron Brown <aaronwbrown@msn.com>
2017-02-10 14:20:40 -0700

    fixing the indentation from adding throttle

--- drive.py
+++ drive.py
@@ -36,12 +36,12 @@ def telemetry(sid, data):
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
         min_speed = 8
         max_speed = 10
-		if float(speed) < min_speed:
-			throttle = 1.0
-		elif float(speed) > max_speed:
-			throttle = -1.0
-		else:
-			throttle = 0.1
+        if float(speed) < min_speed:
+            throttle = 1.0
+        elif float(speed) > max_speed:
+            throttle = -1.0
+        else:
+            throttle = 0.1
         
         print(steering_angle, throttle)
         send_control(steering_angle, throttle)
