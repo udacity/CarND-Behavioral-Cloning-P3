50da854017be3c45bb9c581bf711366710385748
Sagun Man Singh Shrestha <sagunms@msn.com>
2017-02-12 03:30:00 +1000

    Include preprocess_image from the model for drive.py to work.

--- drive.py
+++ drive.py
@@ -16,11 +16,15 @@ from keras.models import load_model
 import h5py
 from keras import __version__ as keras_version
 
+# Fix error with Keras and TensorFlow
+import tensorflow as tf
+tf.python.control_flow_ops = tf
+from model import preprocess_image
+
 sio = socketio.Server()
 app = Flask(__name__)
 model = None
 prev_image_array = None
-
 
 @sio.on('telemetry')
 def telemetry(sid, data):
@@ -31,11 +35,19 @@ def telemetry(sid, data):
         throttle = data["throttle"]
         # The current speed of the car
         speed = data["speed"]
+        speed = float(speed)
         # The current image from the center camera of the car
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
-        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
+
+        # Add the preprocessing step
+        image_array = preprocess_image(image_array)
+
+        transformed_image_array = image_array[None, :, :, :]
+        # This model currently assumes that the features of the model are just the images. Feel free to change this.
+        steering_angle = float(model.predict(transformed_image_array, batch_size=1))
+
         min_speed = 8
         max_speed = 10
         if float(speed) < min_speed:
@@ -44,7 +56,10 @@ def telemetry(sid, data):
             throttle = -1.0
         else:
             throttle = 0.1
-        
+
+        # Faster alternative
+        # throttle = (17.0 - speed) * 0.5
+
         print(steering_angle, throttle)
         send_control(steering_angle, throttle)
 
