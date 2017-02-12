ba161f5add1b1c3e126a1f4d3504be80b4aad36f
Josh Chao <Josh.Chao@careerbuilder.com>
2017-02-12 00:16:37 -0500

    Add PI controller to control throttle smoothly

--- drive.py
+++ drive.py
@@ -22,6 +22,32 @@ model = None
 prev_image_array = None
 
 
+class SimplePIController:
+    def __init__(self, Kp, Ki):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.set_point = 0.
+        self.error = 0.
+        self.integral = 0.
+
+    def set_desired(self, desired):
+        self.set_point = desired
+
+    def update(self, measurement):
+        # proportional error
+        self.error = self.set_point - measurement
+
+        # integral error
+        self.integral += self.error
+
+        return self.Kp * self.error + self.Ki * self.integral
+
+
+controller = SimplePIController(0.1, 0.002)
+set_speed = 15
+controller.set_desired(set_speed)
+
+
 @sio.on('telemetry')
 def telemetry(sid, data):
     if data:
@@ -36,15 +62,9 @@ def telemetry(sid, data):
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
-        min_speed = 8
-        max_speed = 10
-        if float(speed) < min_speed:
-            throttle = 1.0
-        elif float(speed) > max_speed:
-            throttle = -1.0
-        else:
-            throttle = 0.1
-        
+
+        throttle = controller.update(float(speed))
+
         print(steering_angle, throttle)
         send_control(steering_angle, throttle)
 
@@ -97,8 +117,8 @@ if __name__ == '__main__':
 
     if model_version != keras_version:
         print('You are using Keras version ', keras_version,
-            ', but the model was built using ', model_version)
-        
+              ', but the model was built using ', model_version)
+
     model = load_model(args.model)
 
     if args.image_folder != '':
