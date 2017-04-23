6d5a96a4383f2e58c2d4c8710b3a2b6c9280cde9
aponamarev <alex.ponamaryov@gmail.com>
2017-04-23 02:11:22 +0000

    trained modified Lenet

--- train_net.py
+++ train_net.py
@@ -1,5 +1,5 @@
 import os
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import pandas as pd
 
 from src.utils.general_utils import rebalanced_set, continuous_to_bins, generate_data_from, create_train_val_from
@@ -25,11 +25,11 @@ desriptor = pd.read_csv(os.path.join(data_path, csv_file_name))
 create_paths_to_images = lambda x: [os.path.join(data_path, v) for v in x]
 # split the dataset into training and validation data
 
-n, bins, patches = plt.hist(desriptor.steering, bins=n_bins, color='grey')
+#n, bins, patches = plt.hist(desriptor.steering, bins=n_bins, color='grey')
 
-plt.xlabel('Classes')
-plt.ylabel('Samples')
-plt.show()
+#plt.xlabel('Classes')
+#plt.ylabel('Samples')
+#plt.show()
 
 Y_train_binned = continuous_to_bins(desriptor.steering, n_bins=n_bins)
 binned_indices = rebalanced_set(Y_train_binned)
@@ -40,9 +40,13 @@ print("Training set size: {}, Validation set size: {}".format(len(train_indices)
 
 model = Sequential()
 model.add(Lambda(lambda x: (x/255.0)-0.5, input_shape=input_img_shape))
-model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(Convolution2D(6, (5, 5), activation="relu"))
 model.add(MaxPooling2D())
-model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(Convolution2D(16, (5, 5), activation="relu"))
+model.add(MaxPooling2D())
+model.add(Convolution2D(32, (3, 3), activation="relu"))
+model.add(Convolution2D(32, (3, 3), activation="relu"))
+model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dense(120))
 model.add(Dropout(0.25))
@@ -57,10 +61,10 @@ paths_to_images = create_paths_to_images(desriptor.center)
 
 
 model.fit_generator(generate_data_from(train_indices, paths_to_images, desriptor.steering, batch_size),
-                    samples_per_epoch=len(train_indices),
-                    nb_epoch=10,
+                    samples_per_epoch=len(train_indices)//batch_size,
+                    nb_epoch=4,
                     validation_data=generate_data_from(train_indices, paths_to_images, desriptor.steering, batch_size),
-                    nb_val_samples=len(val_indices))
+                    nb_val_samples=len(val_indices)//batch_size)
 
 
 
