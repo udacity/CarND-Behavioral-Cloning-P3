4217860fbf7c6b3b6a12b37c92ee74e6ad199e51
Alex Ponamarev <alex.ponamaryov@gmail.com>
2017-04-30 21:55:47 -0400

    Updated batch norm in Simplified model

--- src/Models/InceptionInspired.py
+++ src/Models/InceptionInspired.py
@@ -1,5 +1,5 @@
 from keras.models import Model
-from keras.layers import Input, InputLayer
+from keras.layers import Input
 from keras.layers.core import Flatten, Dense, Dropout, SpatialDropout2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers import Lambda, Cropping2D
@@ -13,5 +13,12 @@ def InceptionInspired(FLAGS, input_img_shape):
     assert FLAGS.top_crop is not None, "FLAGS.top_crop wasn't provided"
     assert FLAGS.bottom_crop is not None, "FLAGS.bottom_crop wasn't provided"
     assert FLAGS.width is not None, "FLAGS.width wasn't provided"
+
+    X = Input(shape=input_img_shape)
+    X = Lambda(lambda x: (x / 255.0) - 0.5)(X)
+    axis = bn_axis, scale = False
+    BatchNormalization(axis=-1, scale=False)
+
+
 
 
