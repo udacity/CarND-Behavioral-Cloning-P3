8a37abc1d6c57b06b5293dedde84870f25937357
bin <bin@ngs.tech>
2017-08-11 19:16:49 +0800

    update

--- model.py
+++ model.py
@@ -1,33 +1,52 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Convolution2D
+from keras.models import Sequential, Model
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
+import matplotlib.pyplot as plt
 
 
-model = Sequential()
-model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
-model.add(Cropping2D(cropping=((50,20), (0,0))))
-model.add()
+def build_model():
+    model = Sequential()
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+    model.add(Cropping2D(cropping=((75, 25), (0, 0))))
+    model.add(Convolution2D(6, 5, 5, activation="relu"))
+    model.add(MaxPooling2D())
+    model.add(Convolution2D(6, 5, 5, activation="relu"))
+    model.add(MaxPooling2D())
+    model.add(Flatten())
+    model.add(Dense(120))
+    model.add(Dense(84))
+    model.add(Dense(11))
+
+    return model
 
 
-model.add(Flatten())
-model.add(Dense(1))
-
-##########
-
-model.add(Convolution2D(6, 5, 5, activation="relu"))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6, 5, 5, activation="relu"))
-model.add(MaxPooling2D())
-model.add(Flatten())
-model.add(Dense(120))
+def compile_model(model):
+    model.compile(loss='mse', optimizer='adam')
 
 
+def train_model(model, X_data, y_data, epochs):
+    model.fit(X_data, y_data, validation_split=0.2, shuffle=True, nb_epoch=epochs)
+    model.save('model.h5')
 
-model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
 
-model.save('model.h5')
+history_object = model.fit_generator(train_generator, samples_per_epoch =
+    len(train_samples), validation_data =
+    validation_generator,
+    nb_val_samples = len(validation_samples),
+    nb_epoch=5, verbose=1)
+
+### print the keys contained in the history object
+print(history_object.history.keys())
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
