1068ed145c1b74fc3f0f1c1a9ce3f18c774f560d
Yummy Bian <yummy.bian@gmail.com>
2017-08-14 07:49:16 +0800

    update

--- model.py
+++ model.py
@@ -9,9 +9,9 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 
-def build_model(args):
+def build_model(keep_prob):
     """
-    Modified NVIDIA model
+    Build NVIDIA model
     """
     model = Sequential()
     model.add(Lambda(lambda x: x/127.5-1.0, input_shape=INPUT_SHAPE))
@@ -20,44 +20,13 @@ def build_model(args):
     model.add(Conv2D(48, 5, 5, activation='relu', subsample=(2, 2)))
     model.add(Conv2D(64, 3, 3, activation='relu'))
     model.add(Conv2D(64, 3, 3, activation='relu'))
-    model.add(Dropout(args.keep_prob))
+    model.add(Dropout(keep_prob))
     model.add(Flatten())
-    model.add(Dense(100, activation='elu'))
-    model.add(Dense(50, activation='elu'))
-    model.add(Dense(10, activation='elu'))
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(50, activation='relu'))
+    model.add(Dense(10, activation='relu'))
     model.add(Dense(1))
     model.summary()
-
-    return model
-
-
-def build_model():
-    // refer to Nvidia modle
-    model = Sequential()
-    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
-    model.add(Convolution2D(3, 5, 5, activation="relu"), sub)
-    model.add(MaxPooling2D())
-    model.add(Convolution2D(6, 5, 5, activation="relu"))
-    model.add(MaxPooling2D())
-    model.add(Flatten())
-    model.add(Dense(120))
-    model.add(Dense(84))
-    model.add(Dense(11))
-
-    return model
-
-def build_model():
-    // refer to Nvidia modle
-    model = Sequential()
-    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
-    model.add(Convolution2D(6, 5, 5, activation="relu"))
-    model.add(MaxPooling2D())
-    model.add(Convolution2D(6, 5, 5, activation="relu"))
-    model.add(MaxPooling2D())
-    model.add(Flatten())
-    model.add(Dense(120))
-    model.add(Dense(84))
-    model.add(Dense(11))
 
     return model
 
@@ -78,8 +47,10 @@ history_object = model.fit_generator(train_generator,
                                      nb_epoch=5,
                                      verbose=1)
 
+
 ### print the keys contained in the history object
 print(history_object.history.keys())
+
 
 ### plot the training and validation loss for each epoch
 plt.plot(history_object.history['loss'])
@@ -90,21 +61,9 @@ plt.xlabel('epoch')
 plt.legend(['training set', 'validation set'], loc='upper right')
 plt.show()
 
-#############
+# 1. build model
 
+# 2. fit model on training data
 
-ch, row, col = 3, 80, 320  # Trimmed image format
-
-model = Sequential()
-# Preprocess incoming data, centered around zero with small standard deviation
-model.add(Lambda(lambda x: x/127.5 - 1.,
-        input_shape=(ch, row, col),
-        output_shape=(ch, row, col)))
-model.add(... finish defining the rest of your model architecture here ...)
-
-model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator,
-                    samples_per_epoch=len(train_samples),
-                    validation_data=validation_generator,
-                    nb_val_samples=len(validation_samples),
-                    nb_epoch=3)
+# 3. Evaluate
+score = model.evaluate(X_test, Y_test, verbose=0)
