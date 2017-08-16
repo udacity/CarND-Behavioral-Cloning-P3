0a4723f2a9b35122e9e2bb86bde049d46fcb0230
Timothy Smith <tsmith@saberastro.com>
2017-08-16 14:38:27 +1000

    Basic LeNet driving OK, but not handling corners so well

--- model.py
+++ model.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
+
 import os
 
 import numpy as np
 import pandas as pd
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Conv2D, InputLayer, Dropout, MaxPool2D, Cropping2D
+from keras.layers import Dense, Flatten, Conv2D, InputLayer, Dropout, MaxPool2D, Cropping2D, Lambda
 
 import cv2
 
@@ -11,8 +13,10 @@ import cv2
 def build_model():
     image_shape = (160, 320, 3)
     model = Sequential()
-    model.add(Cropping2D(input_shape=image_shape, cropping=((50, 20), (0, 0))))
-    model.add(Conv2D(6, kernel_size=(5, 5), activation='relu'))
+    model.add(InputLayer(input_shape=image_shape))
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+    model.add(Cropping2D(cropping=((50, 20), (0, 0))))
+    model.add(Conv2D(18, kernel_size=(5, 5), activation='relu'))
     model.add(MaxPool2D())
     model.add(Dropout(0.75))
 
@@ -40,12 +44,12 @@ def train(model, x, y):
 def load_driving_log(filename):
     prefix = os.path.split(filename)[0]
     log = pd.DataFrame.from_csv(filename, header=0, index_col=None)
-    images = np.zeros([log.shape[0], 160, 320, 3])
+    images = np.zeros([log.shape[0], 160, 320, 3], dtype=np.uint8)
     for i, path in enumerate(log['center']):
         image = cv2.imread(os.path.join(prefix, path))
         images[i, :, :, :] = image
 
-    return images, log['steering']
+    return images, np.array(log['steering'])
 
 
 def main():
