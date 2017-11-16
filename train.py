c18411ff2714dd7b4439aaabb22ca6867efe8efb
Sara Collins <datadivallc@gmail.com>
2017-11-16 14:26:33 -0600

    Working version for grid search.

--- train.py
+++ train.py
@@ -2,14 +2,17 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Activation, Dropout
 import sys
 import argparse
 import os
 from keras.wrappers.scikit_learn import KerasRegressor
-from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import cross_val_score
-
+from sklearn.model_selection import GridSearchCV
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+import json
+from keras.wrappers.scikit_learn import BaseWrapper
+import copy
 
 
 def parse_args(arguments):
@@ -20,8 +23,8 @@ def parse_args(arguments):
     """
     parser = argparse.ArgumentParser(description="Trains a behavioral cloning model from a given training file set.")
     parser.add_argument('-i', '--input', help="Input run name starter stem. Will be appropriately split to open the driving log and the image directory.",
-                        required=True, dest='input_path')
-    parser.add_argument('-o', '--output', help="Output destination to save model.", required=True, dest='output_path')
+                        dest='input_path')
+    parser.add_argument('-o', '--output', help="Output destination to save model.", dest='output_path')
     parser.add_argument('-l', '--loss', choices=['mse','mean_absolute_error',
                                                  'mean_squared_logarithmic_error',
                                                  'squared_hinge','hinge','categorical_hinge',
@@ -50,6 +53,15 @@ def parse_args(arguments):
     parser.add_argument('-f', '--folds', help='Number of folds to use for k-crossfolds validation.',
                         dest='folds')
     return vars(parser.parse_args(arguments))
+
+
+def load_config(config_name):
+    """
+    loads a json config file and returns a config dictionary
+    """
+    with open(config_name) as config_file:
+        configuration = json.load(config_file)
+        return configuration
 
 def get_log_lines(path):
     """
@@ -85,41 +97,90 @@ def get_images_and_measurements(path, lines):
 
     return np.array(images), np.array(measurements)
 
-def create_model(units=1, loss_function='mse', optimizer='adam', input_shape=(160,320,3)):
+def create_model(units=1, loss_function='mse', optimizer='adam', epochs=10, input_shape=(160,320,3)):
     """
     Constructs Keras model object
     :return: Compiled Keras model object
     """
     model = Sequential()
-    model.add(Flatten(input_shape=input_shape))
+    model.add(Convolution2D(160, 3, 3, input_shape=input_shape))
+    model.add(MaxPooling2D((2,2)))
+    model.add(Dropout(0.5))
+    model.add(Activation('relu'))
+    model.add(Flatten())
     model.add(Dense(units))
 
-    model.compile(loss=loss_function, optimizer=optimizer)
+    model.compile(loss=loss_function, optimizer=optimizer, metrics=['accuracy'])
 
     return model
 
+def custom_get_params(self, **params):
+    """
+    Function to patch issue in Keras
+    :param self:
+    :param params:
+    :return:
+    """
+    res = copy.deepcopy(self.sk_params)
+    res.update({'build_fn': self.build_fn})
+    return res
+
+
 
 if __name__ == '__main__':
+
+    BaseWrapper.get_params = custom_get_params
 
     # Set TensorFlow logging so it isn't so verbose.
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
     args = parse_args(sys.argv[1:])
+    if args['config']:
+        config = load_config(args['config'])
+        lines = get_log_lines(config['input_path'])
+        images, measurements = get_images_and_measurements(config['input_path'], lines)
+        use_grid_search = config['use_grid_search']
 
-    lines = get_log_lines(args['input_path'])
+        X_train = images
+        y_train = measurements
 
-    images, measurements = get_images_and_measurements(args['input_path'], lines)
+        if use_grid_search == 'True':
+            model = KerasRegressor(build_fn=create_model, verbose=0)
 
-    X_train = images
-    y_train = measurements
+            epochs = config["epochs"]
 
-    if args['use_grid_search'] == 'True':
-        model = KerasRegressor(build_fn=create_model, epochs=args['num_epochs'])
-        kfold = StratifiedKFold(n_splits=args['folds'], shuffle=args['shuffle'], random_state=73)
-        results = cross_val_score(model, X_train, y_train, cv=kfold)
-        print("Cross validation result: " + str(results.mean()))
+            param_grid = dict(epochs=epochs)
+
+            grid = GridSearchCV(estimator=model, param_grid=param_grid)
+
+            grid_result = grid.fit(X_train, y_train)
+
+            print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+            means = grid_result.cv_results_['mean_test_score']
+            stds = grid_result.cv_results_['std_test_score']
+            params = grid_result.cv_results_['params']
+            for mean, stdev, parameters in zip(means, stds, params):
+                print("%f (%f) with: %r" % (mean, stdev, parameters))
+        else:
+            model = create_model(config['units'][0])
+            model.fit(X_train, y_train, nb_epoch=config['epochs'][0],
+                      validation_split=0.5, shuffle=True)
+            if config['output_path'].endswith('.h5'):
+                model.save(config['output_path'])
+            else:
+                model.save(config['output_path'] + '.h5')
+
+
     else:
-        model = create_model(args['units'], args['loss_function'], args['optimizer'])
+        lines = get_log_lines(args['input_path'])
+
+        images, measurements = get_images_and_measurements(args['input_path'], lines)
+        use_grid_search = args['use_grid_search']
+
+        X_train = images
+        y_train = measurements
+
+        model = create_model(args['units'])
         model.fit(X_train, y_train, nb_epoch=args['num_epochs'],
                   validation_split=args['validation_split'], shuffle=args['shuffle'])
 
