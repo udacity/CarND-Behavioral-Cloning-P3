c6e6fd41540f9543031d3a12930120ae89ffe67d
Andrew Bauman <baumanab@gmail.com>
2017-12-04 15:26:54 -0800

    Fixes for robustness with respect to different OS
    - load clips as a filtered list to prevent exceptions from non-image files, including system files (OSX) 
    - enable two versions of filenames for wites, version 1 does not work in all environments

--- video.py
+++ video.py
@@ -1,5 +1,8 @@
 from moviepy.editor import ImageSequenceClip
 import argparse
+import os
+
+IMAGE_EXT = ['jpeg', 'gif', 'png', 'jpg']
 
 
 def main():
@@ -17,10 +20,23 @@ def main():
         help='FPS (Frames per second) setting for the video.')
     args = parser.parse_args()
 
-    video_file = args.image_folder + '.mp4'
-    print("Creating video {}, FPS={}".format(video_file, args.fps))
-    clip = ImageSequenceClip(args.image_folder, fps=args.fps)
-    clip.write_videofile(video_file)
+    #convert file folder into list firltered for image file types
+    image_list = sorted([os.path.join(args.image_folder, image_file)
+                        for image_file in os.listdir(args.image_folder)])
+    
+    image_list = [image_file for image_file in image_list if os.path.splitext(image_file)[1][1:].lower() in IMAGE_EXT]
+
+    #two methods of naming output video to handle varying environemnts
+    video_file_1 = args.image_folder + '.mp4'
+    video_file_2 = args.image_folder + 'output_video.mp4'
+
+    print("Creating video {}, FPS={}".format(args.image_folder, args.fps))
+    clip = ImageSequenceClip(image_list, fps=args.fps)
+    
+    try:
+        clip.write_videofile(video_file_1)
+    except:
+        clip.write_videofile(video_file_2)
 
 
 if __name__ == '__main__':
