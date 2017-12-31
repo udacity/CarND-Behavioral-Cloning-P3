1e75d19a8d97b9dbec4be8466a42c6db946c1d12
Sara Collins <datadivallc@gmail.com>
2017-12-30 19:23:02 -0600

    Committing before trying on floyd.

--- train.py
+++ train.py
@@ -383,48 +383,49 @@ def generate_training_data_by_batch(center_images, left_images, right_images, me
     :param batch_size:
     :return:
     """
-    batch_images = np.zeros((batch_size, height, width, channels))
-    batch_measurements = np.zeros(batch_size)
+    while 1:
+        batch_images = np.zeros((batch_size, height, width, channels))
+        batch_measurements = np.zeros(batch_size)
 
-    # TODO: Figure out, do I shuffle beforehand? Also, do I... yeah, do I count the batch size as part of the images I've got?
+        # TODO: Figure out, do I shuffle beforehand? Also, do I... yeah, do I count the batch size as part of the images I've got?
 
-    for batch_index in range(0, len(measurements) // batch_size):
-        starting_index_for_batch = batch_index * batch_size
-        ending_index_for_batch = starting_index_for_batch + batch_size
+        for batch_index in range(0, len(measurements) // batch_size):
+            starting_index_for_batch = batch_index * batch_size
+            ending_index_for_batch = starting_index_for_batch + batch_size
 
-        batch_center = center_images[starting_index_for_batch:ending_index_for_batch]
-        batch_left = left_images[starting_index_for_batch:ending_index_for_batch]
-        batch_right = right_images[starting_index_for_batch:ending_index_for_batch]
-        batch_measurements = measurements[starting_index_for_batch:ending_index_for_batch]
+            batch_center = center_images[starting_index_for_batch:ending_index_for_batch]
+            batch_left = left_images[starting_index_for_batch:ending_index_for_batch]
+            batch_right = right_images[starting_index_for_batch:ending_index_for_batch]
+            batch_measurements = measurements[starting_index_for_batch:ending_index_for_batch]
 
-        final_batch_images = []
-        final_batch_measurements = []
+            final_batch_images = []
+            final_batch_measurements = []
 
-        # Augment the images
-        for i in range(len(batch_center)):
-            index_images = []
-            index_measurements = []
+            # Augment the images
+            for i in range(len(batch_center)):
+                index_images = []
+                index_measurements = []
 
-            center_images, center_measurements = augment_image(batch_center[i], 'center', batch_measurements[i])
-            [index_images.append(img) for img in center_images]
-            [index_measurements.append(mmt) for mmt in center_measurements]
+                center_images, center_measurements = augment_image(batch_center[i], 'center', batch_measurements[i])
+                [index_images.append(img) for img in center_images]
+                [index_measurements.append(mmt) for mmt in center_measurements]
 
-            left_images, left_measurements = augment_image(batch_left[i], 'left', batch_measurements[i])
-            [index_images.append(img) for img in left_images]
-            [index_measurements.append(mmt) for mmt in left_measurements]
+                left_images, left_measurements = augment_image(batch_left[i], 'left', batch_measurements[i])
+                [index_images.append(img) for img in left_images]
+                [index_measurements.append(mmt) for mmt in left_measurements]
 
-            right_images, right_measurements = augment_image(batch_right[i], 'right', batch_measurements[i])
-            [index_images.append(img) for img in right_images]
-            [index_measurements.append(mmt) for mmt in right_measurements]
+                right_images, right_measurements = augment_image(batch_right[i], 'right', batch_measurements[i])
+                [index_images.append(img) for img in right_images]
+                [index_measurements.append(mmt) for mmt in right_measurements]
 
-            # Pick a random image out of each of the augmented images. Keeps the batch size in check.
-            random_index = np.random.randint(len(index_measurements))
+                # Pick a random image out of each of the augmented images. Keeps the batch size in check.
+                random_index = np.random.randint(len(index_measurements))
 
-            final_batch_images.append(crop_image(index_images[random_index], horizon_divisor=5, hood_pixels=25,
-                                                 crop_height=64, crop_width=64))
-            final_batch_measurements.append(index_measurements[random_index])
+                final_batch_images.append(crop_image(index_images[random_index], horizon_divisor=5, hood_pixels=25,
+                                                     crop_height=height, crop_width=width))
+                final_batch_measurements.append(index_measurements[random_index])
 
-    yield batch_images, batch_measurements
+        yield batch_images, batch_measurements
 
 
 if __name__ == '__main__':
@@ -446,12 +447,20 @@ if __name__ == '__main__':
         get_all_images_and_measurements(lines, old_root=config['old_image_root'],
                                                            new_root=config['new_image_root'])
 
-    # Designate X and y data
-    X_train = center_images
-    y_train = measurements
+    # Designate validation
+    validation_index = int(len(measurements) * .2)
+    center_images_valid = center_images[-validation_index]
+    left_images_valid = left_images[-validation_index]
+    right_images_valid = right_images[-validation_index]
+    measurements_valid = measurements[-validation_index]
+
+    center_images = center_images[:-validation_index]
+    left_images = left_images[:-validation_index]
+    right_images = right_images[:-validation_index]
+    measurements = measurements[:-validation_index]
 
     model = create_model(config['units'], gpus=config['gpus'], learning_rate=config['learning_rate'])
-    ckpt_path = "/output/floyd_model_1_{epoch:02d}_{val_acc:.2f}.hdf5"
+    ckpt_path = "/output/floyd_model_augment_2_{epoch:02d}_{val_acc:.2f}.hdf5"
     checkpointer = ModelCheckpoint(ckpt_path, verbose=1, save_best_only=True)
 
     # Establish tensorboard
@@ -462,8 +471,19 @@ if __name__ == '__main__':
     else:
         callbacks = [checkpointer]
 
-    model.fit(X_train, y_train, nb_epoch=config['epochs'], batch_size=config['batch_size'],
-              validation_split=0.2, shuffle=True, callbacks=callbacks)
+    #model.fit(X_train, y_train, nb_epoch=config['epochs'], batch_size=config['batch_size'],
+    #          validation_split=0.2, shuffle=True, callbacks=callbacks)
+
+    model.fit_generator(generate_training_data_by_batch(center_images, left_images, right_images, measurements,
+                                                        batch_size=config['batch_size']),
+                        steps_per_epoch=(len(center_images) // config['batch_size']), epochs=config['epochs'],
+                        validation_data=generate_training_data_by_batch(center_images_valid,
+                                                                                                   left_images_valid,
+                                                                                                   right_images_valid,
+                                                                                                   measurements_valid,
+                                                                                                   batch_size=1),
+                        shuffle=True, callbacks=callbacks)
+
 
     if config['output_path'].endswith('.h5'):
         model.save(config['output_path'])
