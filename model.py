3915664d725f2fbcc432d41d44c25a074a2621f7
Yuma Hiramatsu <ayzgnm5k8y@gmail.com>
2018-10-21 18:03:07 +0900

    wip

--- model.py
+++ model.py
@@ -2,16 +2,17 @@ import csv
 import cv2
 import numpy as np
 from pathlib import Path
-
+from sklearn import utils
+from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
-def return_model():
+def get_model():
     model = Sequential()
 
-    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+    model.add(Lambda(lambda x: x / 127.5 - 1., input_shape=(160, 320, 3)))
     model.add(Cropping2D(cropping=((70, 25), (0, 0))))
     model.add(Convolution2D(24, (5, 5), strides=(2, 2), activation='relu'))
     model.add(Convolution2D(36, (5, 5), strides=(2, 2), activation='relu'))
@@ -27,40 +28,53 @@ def return_model():
 
     return model
 
-lines = []
-csv_path = Path('./data/driving_log.csv')
-with csv_path.open() as csvfile:
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        utils.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                for pos in range(3):
+                    file_name = Path(batch_sample[pos]).name
+                    image_path = Path('./data/IMG').joinpath(file_name)
+                    images.append(cv2.imread(image_path.as_posix()))
+
+                center_angle = float(batch_sample[6])
+                correction = 0.2
+                angles.extend([
+                    center_angle,
+                    center_angle + correction,
+                    center_angle - correction
+                ])
+
+            for i in range(len(images)):
+                images.append(cv2.flip(images[i], 1))
+                angles.append(-angles[i])
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield utils.shuffle(X_train, y_train)
+
+samples = []
+with Path('./data/driving_log.csv').open() as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
-        lines.append(line)
+        samples.append(line)
 
-images = []
-angles = []
-for line in lines:
-    for pos in range(3):
-        file_name = Path(line[pos]).name
-        image_path = Path('./data/IMG').joinpath(file_name)
-        images.append(cv2.imread(image_path.as_posix()))
+train_samples, valid_samples = train_test_split(samples, test_size=0.2)
 
-    steering_center = float(line[3])
-    correction = 0.2
-    angles.extend([
-        steering_center,
-        steering_center + correction,
-        steering_center - correction
-    ])
+train_generator = generator(train_samples, batch_size=32)
+valid_generator = generator(valid_samples, batch_size=32)
 
-assert(len(images) == len(angles))
-
-for i in range(len(images)):
-    images.append(cv2.flip(images[i], 1))
-    angles.append(-angles[i])
-
-X_train = np.array(images)
-y_train = np.array(angles)
-
-model = return_model()
+model = get_model()
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
+model.fit_generator(train_generator, steps_per_epoch=len(train_samples),
+                    validation_data=valid_generator,
+                    validation_steps=len(valid_samples), epochs=2)
 
 model.save('model.h5')
