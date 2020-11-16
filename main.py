1368467db8e41abcbe905aa0e5936bc868575f61
KhardanOne <hod64@freemail.hu>
2020-11-16 19:15:44 +0100

    tidy

--- main.py
+++ main.py
@@ -18,13 +18,7 @@ def show_img(image):
     plt.show()
 
 
-def main(index):
-    X_train, y_train = read_sim_data(index)
-    # show_img(X_train[0])
-
-    print(X_train.shape)
-    print(y_train.shape)
-
+def lenet(x, y):
     model = Sequential()
     model.add(Lambda(lambda x: x - 255.0 - 0.5, input_shape=(160, 320, 3)))
     model.add(Conv2D(filters=16, kernel_size=(7,7), strides=(2,2), padding='same'))  # 160x320x16
@@ -44,8 +38,19 @@ def main(index):
     model.add(Dense(1))
 
     model.compile(loss='mse', optimizer='adam')
-    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=15)
+    model.fit(x, y, validation_split=0.2, shuffle=True, epochs=15)
     model.save(CFG.path_model)
+
+
+
+def main(index):
+    X_train, y_train = read_sim_data(index)
+    # show_img(X_train[0])
+
+    print(X_train.shape)
+    print(y_train.shape)
+
+    lenet(X_train, y_train)
 
 
 if __name__ == '__main__':
