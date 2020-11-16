838693459dfde0c03c490afc0a47feb41a62da15
KhardanOne <hod64@freemail.hu>
2020-11-16 19:11:34 +0100

    A few hundred successfull meters

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from cfg import *
 from reader import read_sim_data
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPool2D, Activation
 import matplotlib.pyplot as plt
 import os
 
@@ -27,7 +27,20 @@ def main(index):
 
     model = Sequential()
     model.add(Lambda(lambda x: x - 255.0 - 0.5, input_shape=(160, 320, 3)))
+    model.add(Conv2D(filters=16, kernel_size=(7,7), strides=(2,2), padding='same'))  # 160x320x16
+    model.add(MaxPool2D((4,4)))  # 40x80x16
+    model.add(Activation('relu'))
+    model.add(Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), padding='same'))
+    model.add(MaxPool2D((4,4)))  # 10*20*64
+    model.add(Activation('relu'))
+    model.add(Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), padding='same'))
+    model.add(MaxPool2D((2,2)))  # 5*10*256
+    model.add(Activation('relu'))
     model.add(Flatten())
+    model.add(Dense(1000))
+    model.add(Activation('relu'))
+    model.add(Dense(200))
+    model.add(Activation('relu'))
     model.add(Dense(1))
 
     model.compile(loss='mse', optimizer='adam')
