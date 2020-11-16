85378ce29394a47e913b98df0e962e8de15214bf
KhardanOne <hod64@freemail.hu>
2020-11-16 18:11:37 +0100

    Added model. drive.py model.h5 works, but it is a dummy.

--- main.py
+++ main.py
@@ -2,20 +2,36 @@ from cfg import *
 from reader import read_sim_data
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda
+import matplotlib.pyplot as plt
+import os
+
+GPU = False # or True
+if GPU:
+    os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+else:
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+
+def show_img(image):
+    plt.imshow(image)
+    plt.show()
 
 
 def main(index):
     X_train, y_train = read_sim_data(index)
+    # show_img(X_train[0])
+
     print(X_train.shape)
     print(y_train.shape)
 
     model = Sequential()
-    model.add(Flatten(input_shape=(160, 320, 3)))
+    model.add(Lambda(lambda x: x - 255.0 - 0.5, input_shape=(160, 320, 3)))
+    model.add(Flatten())
     model.add(Dense(1))
 
     model.compile(loss='mse', optimizer='adam')
-    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=7)
+    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=15)
     model.save(CFG.path_model)
 
 
--- reader.py
+++ reader.py
@@ -6,6 +6,7 @@ import numpy as np
 
 
 def read_sim_data(index):
+    # read csv
     path_current_root = CFG.path_fmt_data_root.format(index)
     path = path_current_root + CFG.path_log
     print('Reading file {}...'.format(path))
@@ -16,6 +17,7 @@ def read_sim_data(index):
             csv_lines.append(line)
     print('File read successfully.', csv_lines[0])
 
+    # read images
     print('Reading images...')
     images = []
     steering_measurements = []
@@ -26,16 +28,16 @@ def read_sim_data(index):
         current_path = path_current_root + CFG.path_img + filename
         if os.path.exists(current_path):
             image = cv2.imread(current_path)
+            image = image[...,::-1]
             images.append(image)
             steering_measurement = float(line[3])
             steering_measurements.append(steering_measurement)
             success += 1
         else:
             print('WARNING! File missing: {}'.format(current_path))
-
     images = np.array(images)
     steering_measurements = np.array(steering_measurements)
-    print('From a total of {} successully read {} images.'.format(len(csv_lines), success))
+    print('From a total of {} successfully read {} images.'.format(len(csv_lines), success))
     return images, steering_measurements
 
 
