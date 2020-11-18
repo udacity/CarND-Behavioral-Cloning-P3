1eaca92656e6a45f42ec55496f499cd0f270c7ae
KhardanOne <hod64@freemail.hu>
2020-11-18 21:04:01 +0100

    Added videos. The reverse videos are not full, as the connection got lost.

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 """
 Improvement ideas:
 - Use screenshots from several runs
-- Use challenge videos
 - Use recovery videos
 - Use reverse videos
 - Gather smoother steering input (mouse, wheel?)
@@ -10,16 +9,16 @@ Improvement ideas:
 - HSL: emphasize colors
 - nVidia algorithm
 - Add throttle / brake
-- If too much input: then use a generator function with with .fit_generator()
+- If too much input: use a generator function with with .fit_generator()
 """
 
+import os
 import cfg
 import reader
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPool2D, Activation, Dropout, Cropping2D
 import matplotlib.pyplot as plt
-import os
 import data_manip
 
 
@@ -34,7 +33,7 @@ def show_img(image):
     plt.show()
 
 
-def lenet_v1(x, y):
+def build_model(x, y):
     model = Sequential()
     model.add(Cropping2D(((30, 20), (0, 0)), input_shape=(160, 320, 3)))
     model.add(Lambda(lambda x: x / 255.0 - 0.5))
@@ -47,7 +46,8 @@ def lenet_v1(x, y):
     model.add(Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), padding='same'))
     model.add(MaxPool2D((2,2)))  # 5*10*256
     model.add(Activation('relu'))
-    model.add(Flatten())
+    model.add(Flatten())  #
+    model.add(Dropout(0.3))
     model.add(Dense(1000))
     model.add(Activation('relu'))
     model.add(Dense(200))
@@ -55,36 +55,7 @@ def lenet_v1(x, y):
     model.add(Dense(1))
 
     model.compile(loss='mse', optimizer='adam')
-    model.fit(x, y, validation_split=0.2, shuffle=True, epochs=3)
-    model.save(cfg.path_model)
-
-
-def lenet_v2(x, y):
-    model = Sequential()
-    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
-    model.add(Conv2D(filters=16, kernel_size=(7,7), strides=(2,2), padding='same'))  # 160x320x16
-    model.add(MaxPool2D((4,4)))  # 40x80x16
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    model.add(Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), padding='same'))
-    model.add(MaxPool2D((4,4)))  # 10*20*64
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    model.add(Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), padding='same'))
-    model.add(MaxPool2D((2,2)))  # 5*10*256
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    model.add(Flatten())
-    model.add(Dense(1000))
-    model.add(Dropout(0.5))
-    model.add(Activation('relu'))
-    model.add(Dense(200))
-    model.add(Dropout(0.5))
-    model.add(Activation('relu'))
-    model.add(Dense(1))
-
-    model.compile(loss='mse', optimizer='adam')
-    model.fit(x, y, validation_split=0.2, shuffle=True, epochs=3)
+    model.fit(x, y, validation_split=0.2, shuffle=True, epochs=2)
     model.save(cfg.path_model)
 
 
@@ -95,7 +66,7 @@ def main(first_dataset, last_dataset, verbose=False):
     if verbose:
         data_manip.show_example(X_train, y_train, start_index=0, columns=3, second_row_offset=len(X_train) // 2)
 
-    lenet_v1(X_train, y_train)
+    build_model(X_train, y_train)
 
 
 if __name__ == '__main__':
